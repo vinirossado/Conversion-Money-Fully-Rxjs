f9f11dcdb2643b74f3e54386af15dad4f51fc66f
Vinicius Rossado <vinirossado@gmail.com>
2022-05-22 20:12:45 +0300

    Feature: Applying quotation expired logi

--- src/app/conversion/conversion/conversion.component.ts
+++ src/app/conversion/conversion/conversion.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject, debounceTime, distinctUntilChanged, EMPTY, iif, map, Observable, of, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, debounceTime, delay, distinctUntilChanged, EMPTY, iif, map, Observable, of, switchMap, tap, timeout } from 'rxjs';
 import { GenerateRateQuotePayloadModel } from 'src/app/models';
 import { ConversionService } from 'src/app/services';
 
@@ -13,12 +13,14 @@ import { ConversionService } from 'src/app/services';
 })
 export class ConversionComponent implements OnInit {
     private static DELAY = 500;
+    trendDirectionUP: boolean = false;
     conversionForm: FormGroup;
+
     sentAmount$: Observable<number>;
     receivedAmount$: Observable<number>;
     loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
     rate$: BehaviorSubject<string> = new BehaviorSubject<string>('');
-    trendDirectionUP: boolean = false;
+    expired$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
     constructor(private _conversionService: ConversionService, private _formBuilder: FormBuilder) {
         this.conversionForm = this._formBuilder.group({
@@ -42,12 +44,14 @@ export class ConversionComponent implements OnInit {
             debounceTime(ConversionComponent.DELAY),
             switchMap(value => iif(() => this.conversionForm.controls['sentAmount'].valid, of(value), EMPTY)),
             tap(() => this.loading$.next(true)),
+            tap(() => this.expired$.next(false)),
             switchMap(value => this._conversionService.convertSentValue(value)),
             tap((response: GenerateRateQuotePayloadModel) => this.updateRate(response.rate)),
-            tap((response: GenerateRateQuotePayloadModel) => this.showQuotationExpiry(response.expiresAt)),
-            map((response: GenerateRateQuotePayloadModel) => response.receivedAmount),
-            tap((response: string) => this.patchRecievedAmountWithoutEmit(response)),
+            tap((response: GenerateRateQuotePayloadModel) => this.showQuotationExpiry(new Date(response.expiresAt))),
+            tap((response: GenerateRateQuotePayloadModel) => this.patchRecievedAmountWithoutEmit(response.receivedAmount)),
             tap(() => this.loading$.next(false)),
+            switchMap((response: GenerateRateQuotePayloadModel) => this.addQuotationDelay(response)),
+            tap(() => this.expired$.next(true)),
         );
     }
 
@@ -56,14 +60,25 @@ export class ConversionComponent implements OnInit {
             distinctUntilChanged(),
             debounceTime(ConversionComponent.DELAY),
             switchMap(value => iif(() => this.conversionForm.controls['receivedAmount'].valid, of(value), EMPTY)),
+            tap(() => this.expired$.next(false)),
             tap(() => this.loading$.next(true)),
             switchMap(value => this._conversionService.convertReceivedValue(value)),
             tap((response: GenerateRateQuotePayloadModel) => this.updateRate(response.rate)),
-            map((response: GenerateRateQuotePayloadModel) => response.sentAmount),
-            tap((response: string) => this.patchSentAmountWithoutEmit(response)),
+            tap((response: GenerateRateQuotePayloadModel) => this.showQuotationExpiry(new Date(response.expiresAt))),
+            tap((response: GenerateRateQuotePayloadModel) => this.patchSentAmountWithoutEmit(response.sentAmount)),
             tap(() => this.loading$.next(false)),
+            switchMap((response: GenerateRateQuotePayloadModel) => this.addQuotationDelay(response)),
+            tap(() => this.expired$.next(true)),
 
         );
+    }
+
+    private addQuotationDelay(value: GenerateRateQuotePayloadModel): Observable<GenerateRateQuotePayloadModel> {
+        const expirationDate = new Date(value.expiresAt);
+        const currentMilliseconds = new Date().getTime();
+        const expiration = expirationDate.getTime() - currentMilliseconds;
+        return of(value).pipe(delay(expiration))
+
     }
 
     private patchRecievedAmountWithoutEmit(sentAmount: string) {
@@ -74,8 +89,9 @@ export class ConversionComponent implements OnInit {
         this.conversionForm.patchValue({ sentAmount: receivedAmount }, { emitEvent: false })
     }
 
-    private showQuotationExpiry(expiresAt: string) {
+    private showQuotationExpiry(expiresAt: Date) {
 
+        console.log(expiresAt)
     }
 
     private updateRate(rate: string) {
