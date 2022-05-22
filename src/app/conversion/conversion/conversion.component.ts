7f9b8a717c3af0f30310ae181a2fa4e2f48b2c03
Vinicius Rossado <vinirossado@gmail.com>
2022-05-22 22:17:40 +0300

    Feature: improving logic

--- src/app/conversion/conversion/conversion.component.ts
+++ src/app/conversion/conversion/conversion.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject, debounceTime, delay, distinctUntilChanged, EMPTY, iif, map, Observable, of, switchMap, tap, timeout } from 'rxjs';
+import { BehaviorSubject, catchError, debounceTime, delay, distinctUntilChanged, EMPTY, iif, map, Observable, of, repeat, retry, Subscription, switchMap, tap, timeout } from 'rxjs';
 import { GenerateRateQuotePayloadModel } from 'src/app/models';
 import { ConversionService } from 'src/app/services';
 
@@ -11,8 +11,8 @@ import { ConversionService } from 'src/app/services';
     changeDetection: ChangeDetectionStrategy.OnPush,
 
 })
-export class ConversionComponent implements OnInit {
-    private static DELAY = 500;
+export class ConversionComponent implements OnInit, OnDestroy {
+    private readonly DELAY: number = 500;
     trendDirectionUP: boolean = false;
     conversionForm: FormGroup;
 
@@ -21,6 +21,7 @@ export class ConversionComponent implements OnInit {
     loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
     rate$: BehaviorSubject<string> = new BehaviorSubject<string>('');
     expired$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    subscription: Subscription = new Subscription();
 
     constructor(private _conversionService: ConversionService, private _formBuilder: FormBuilder) {
         this.conversionForm = this._formBuilder.group({
@@ -31,45 +32,47 @@ export class ConversionComponent implements OnInit {
         this.sentAmount$ = this.conversionForm.controls['sentAmount'].valueChanges.pipe(map(x => +x));
         this.receivedAmount$ = this.conversionForm.controls['receivedAmount'].valueChanges.pipe(map(x => +x));
     }
-
-    ngOnInit() {
-        this.listenSentAmountChange().subscribe();
-        this.listenReceivedAmountChange().subscribe();
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
     }
 
+    ngOnInit() {
+        this.subscription.add(this.listenSentAmountChange().subscribe());
+        this.subscription.add(this.listenReceivedAmountChange().subscribe());
+    }
 
     private listenSentAmountChange() {
         return this.sentAmount$.pipe(
             distinctUntilChanged(),
-            debounceTime(ConversionComponent.DELAY),
+            debounceTime(this.DELAY),
             switchMap(value => iif(() => this.conversionForm.controls['sentAmount'].valid, of(value), EMPTY)),
             tap(() => this.loading$.next(true)),
             tap(() => this.expired$.next(false)),
             switchMap(value => this._conversionService.convertSentValue(value)),
             tap((response: GenerateRateQuotePayloadModel) => this.updateRate(response.rate)),
-            tap((response: GenerateRateQuotePayloadModel) => this.showQuotationExpiry(new Date(response.expiresAt))),
-            tap((response: GenerateRateQuotePayloadModel) => this.patchRecievedAmountWithoutEmit(response.receivedAmount)),
+            tap((response: GenerateRateQuotePayloadModel) => this.patchReceivedAmountWithoutEmit(response.receivedAmount)),
             tap(() => this.loading$.next(false)),
             switchMap((response: GenerateRateQuotePayloadModel) => this.addQuotationDelay(response)),
             tap(() => this.expired$.next(true)),
+            tap(() => console.log('finalizou')),
+
         );
     }
 
     private listenReceivedAmountChange() {
         return this.receivedAmount$.pipe(
             distinctUntilChanged(),
-            debounceTime(ConversionComponent.DELAY),
+            debounceTime(this.DELAY),
             switchMap(value => iif(() => this.conversionForm.controls['receivedAmount'].valid, of(value), EMPTY)),
             tap(() => this.expired$.next(false)),
             tap(() => this.loading$.next(true)),
             switchMap(value => this._conversionService.convertReceivedValue(value)),
             tap((response: GenerateRateQuotePayloadModel) => this.updateRate(response.rate)),
-            tap((response: GenerateRateQuotePayloadModel) => this.showQuotationExpiry(new Date(response.expiresAt))),
             tap((response: GenerateRateQuotePayloadModel) => this.patchSentAmountWithoutEmit(response.sentAmount)),
             tap(() => this.loading$.next(false)),
             switchMap((response: GenerateRateQuotePayloadModel) => this.addQuotationDelay(response)),
             tap(() => this.expired$.next(true)),
-
+            tap(() => console.log('finalizou')),
         );
     }
 
@@ -77,24 +80,19 @@ export class ConversionComponent implements OnInit {
         const expirationDate = new Date(value.expiresAt);
         const currentMilliseconds = new Date().getTime();
         const expiration = expirationDate.getTime() - currentMilliseconds;
-        return of(value).pipe(delay(expiration))
+        return of(value).pipe(delay(10000))
 
     }
 
-    private patchRecievedAmountWithoutEmit(sentAmount: string) {
+    private patchReceivedAmountWithoutEmit(sentAmount: string): void {
         this.conversionForm.patchValue({ receivedAmount: sentAmount }, { emitEvent: false })
     }
 
-    private patchSentAmountWithoutEmit(receivedAmount: string) {
+    private patchSentAmountWithoutEmit(receivedAmount: string): void {
         this.conversionForm.patchValue({ sentAmount: receivedAmount }, { emitEvent: false })
     }
 
-    private showQuotationExpiry(expiresAt: Date) {
-
-        console.log(expiresAt)
-    }
-
-    private updateRate(rate: string) {
+    private updateRate(rate: string): void {
         this.trendDirectionUP = rate > this.rate$.value;
         this.rate$.next(rate);
     }
