1b509bc002c70ef93fa2c6ed513fc821c22c43cc
Vinicius Rossado <vinirossado@gmail.com>
2022-05-22 18:32:54 +0300

    Feature: Implementation of logic

--- src/app/conversion/conversion/conversion.component.ts
+++ src/app/conversion/conversion/conversion.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject, debounceTime, distinctUntilChanged, from, map, Observable, of, Subject, switchMap, tap, UnaryFunction } from 'rxjs';
-import { GenerateRateQuoteModel, GenerateRateQuotePayloadModel } from 'src/app/models';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, EMPTY, iif, map, Observable, of, switchMap, tap } from 'rxjs';
+import { GenerateRateQuotePayloadModel } from 'src/app/models';
 import { ConversionService } from 'src/app/services';
 
 @Component({
@@ -13,15 +13,17 @@ import { ConversionService } from 'src/app/services';
 })
 export class ConversionComponent implements OnInit {
 
-    conversionForm: FormGroup
+    conversionForm: FormGroup;
     sentAmount$: Observable<number>;
     receivedAmount$: Observable<number>;
     loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    rate$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+    trendDirectionUP: boolean = false;
 
     constructor(private _conversionService: ConversionService, private _formBuilder: FormBuilder) {
         this.conversionForm = this._formBuilder.group({
-            sentAmount: [null, Validators.compose([Validators.required, Validators.min(0)])],
-            receivedAmount: [null, Validators.compose([Validators.required, Validators.min(0)])]
+            sentAmount: [null, Validators.compose([Validators.required, Validators.min(0.01)])],
+            receivedAmount: [null, Validators.compose([Validators.required, Validators.min(0.01)])]
         });
 
         this.sentAmount$ = this.conversionForm.controls['sentAmount'].valueChanges.pipe(map(x => +x));
@@ -29,17 +31,21 @@ export class ConversionComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.listenSentAmountChange().subscribe()
-        this.listenReceivedAmountChange().subscribe()
+        this.listenSentAmountChange().subscribe();
+        this.listenReceivedAmountChange().subscribe();
     }
+
 
     private listenSentAmountChange() {
         return this.sentAmount$.pipe(
             distinctUntilChanged(),
             debounceTime(500),
+            switchMap(value => iif(() => this.conversionForm.controls['sentAmount'].valid, of(value), EMPTY)),
             tap(() => this.loading$.next(true)),
             switchMap(value => this._conversionService.convertSentValue(value)),
-            map((response: GenerateRateQuotePayloadModel) => response.sentAmount),
+            tap((response: GenerateRateQuotePayloadModel) => this.updateRate(response.rate)),
+            tap((response: GenerateRateQuotePayloadModel) => this.showQuotationExpiry(response.expiresAt)),
+            map((response: GenerateRateQuotePayloadModel) => response.receivedAmount),
             tap((response: string) => this.patchRecievedAmountWithoutEmit(response)),
             tap(() => this.loading$.next(false)),
         );
@@ -49,11 +55,14 @@ export class ConversionComponent implements OnInit {
         return this.receivedAmount$.pipe(
             distinctUntilChanged(),
             debounceTime(500),
+            switchMap(value => iif(() => this.conversionForm.controls['receivedAmount'].valid, of(value), EMPTY)),
             tap(() => this.loading$.next(true)),
             switchMap(value => this._conversionService.convertReceivedValue(value)),
-            map((response: GenerateRateQuotePayloadModel) => response.receivedAmount),
+            tap((response: GenerateRateQuotePayloadModel) => this.updateRate(response.rate)),
+            map((response: GenerateRateQuotePayloadModel) => response.sentAmount),
             tap((response: string) => this.patchSentAmountWithoutEmit(response)),
             tap(() => this.loading$.next(false)),
+
         );
     }
 
@@ -63,6 +72,15 @@ export class ConversionComponent implements OnInit {
 
     private patchSentAmountWithoutEmit(receivedAmount: string) {
         this.conversionForm.patchValue({ sentAmount: receivedAmount }, { emitEvent: false })
+    }
+
+    private showQuotationExpiry(expiresAt: string) {
+
+    }
+
+    private updateRate(rate: string) {
+        this.trendDirectionUP = rate > this.rate$.value;
+        this.rate$.next(rate);
     }
 }
 
